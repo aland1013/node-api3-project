d687778026488b6da930a736777fbcf3bbd5331f
aland1013 <adam.land@gmail.com>
2020-07-14 22:39:34 -0400

    finished first postRouter endpoint for stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
+const postDb = require('../posts/postDb');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  // this endpoint is the same as the one in the userRouter
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+/* ----- GET /api/users/:userId/posts/:postId ----- */
+router.get('/:postId', validatePostId, (req, res) => {
+  res.status(200).json(req.post);
 });
 
 router.delete('/:id', (req, res) => {
@@ -21,7 +24,24 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const id = req.params.postId;
+  console.log('id', id);
+  postDb
+    .getById(id)
+    .then((post) => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res.status(400).json({
+          message: 'invalid post id'
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: 'exception', err });
+    });
 }
 
 module.exports = router;
