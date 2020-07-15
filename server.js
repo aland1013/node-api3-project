daa8151640a3e043275db6a6a4beb13af7b05b01
aland1013 <adam.land@gmail.com>
2020-07-14 22:00:07 -0400

    mvp complete

--- server.js
+++ server.js
@@ -1,51 +1,27 @@
 const express = require('express');
 const { getById } = require('./users/userDb');
+const userRouter = require('./users/userRouter');
 
 const server = express();
-
 server.use(express.json());
 server.use(logger);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
 function logger(req, res, next) {
-  console.log('method:', req.method, 'url:', req.url, 'timestamp:', new Date());
+  console.log(
+    'method:',
+    req.method,
+    'url:',
+    req.url,
+    'timestamp:',
+    new Date().toLocaleTimeString('en-US')
+  );
   next();
-}
-
-function validateId(req, res, next) {
-  const { id } = req.params;
-  const user = getById(id);
-  if (user) {
-    req.user = user;
-    next();
-  } else {
-    res.status(400).json({ message: 'invalid user id' });
-  }
-}
-
-function validateUser(req, res, next) {
-  if (!req.body) {
-    res.status(400).json({ message: 'missing user data' });
-  } else if (!req.body.name) {
-    res.status(400).json({ message: 'missing required name field' });
-  } else {
-    next();
-  }
-}
-
-function validatePost(req, res, next) {
-  if (!req.body) {
-    res.status(400).json({ message: 'missing post data' });
-  } else if (!req.body.text) {
-    res.status(400).json({ message: 'missing required text field' });
-  } else {
-    next();
-  }
 }
 
 module.exports = server;
